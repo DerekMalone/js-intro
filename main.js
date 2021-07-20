cdcf1d9499661b6226ce51a2cbe96d73edb95235
Derek Malone <laxmalone@gmail.com>
2021-07-19 20:41:43 -0500

    class work

--- main.js
+++ main.js
@@ -1,1 +1,25 @@
-console.log('Testing');
+// const firstName = 'Derek';
+// const lastName = 'Malone';
+const favFood = 'sushi';
+// let favNumber = 1;
+// const likesToRead = true;
+
+// favNumber += 3;
+
+// console.log(favNumber);
+
+
+
+const whatFoodDoILike = () => {
+    if (favFood === 'hot dogs' || favFood === 'Sushi') {
+        console.log(`You like ${favFood}!`);
+    } else {
+        console.log(`you like something else!`);
+    }
+}
+
+const myFunction = (str) => {
+    console.log(`Your argument was: ${str}`);
+}
+
+myFunction('Foolish!')
